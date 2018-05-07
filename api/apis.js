2b409ab2acd199ffa29edd337ebb2d3a4bb40be0
pei.han[韓培倫] <pei.han@104.com.tw>
2018-05-07 15:49:20 +0800

    add private chat

--- api/apis.js
+++ api/apis.js
@@ -1,9 +1,53 @@
-function getTime_Str(){
+function getTime_Str() {
     var date = Date.now()
     //var date = `${date.getUTCFullYear()}-${(date.getUTCMonth()+1)<10?'0'+(date.getUTCMonth()+1):(date.getUTCMonth()+1)}-${date.getUTCDate()<10?'0'+date.getUTCDate():date.getUTCDate()} ${date.getUTCHours()<10?'0'+date.getUTCHours():date.getUTCHours()}:${date.getUTCMinutes()<10?'0'+date.getUTCMinutes():date.getUTCMinutes()}:${date.getUTCSeconds()<10?'0'+date.getUTCSeconds():date.getUTCSeconds()}`
     return date
 }
 
-module.exports={
+function newchatroom(io, nsp,rooms) {
+    (function (io, nsp) {
+        var room = io.of(nsp)
+        room.on('connection', function (socket) {
+            console.log('someone coming')
+            console.log(Object.keys(room.sockets))
+            socket.on('welcome chat', function (nickName) {
+                console.log('random chat welcome!')
+
+                socket['nickName'] = nickName
+                var msg = 'comming...'
+                room.emit('chat message', `${socket.nickName} ${msg}`)
+            })
+            socket.on('chat message', function (msg) {
+                console.log(`catch ${msg}!`)
+                room.emit('chat message', `${socket.nickName} : ${msg}`)
+            })
+
+            socket.on('disconnect', function () {
+                if (Object.keys(room.sockets).length == 0) {
+                    const connectedNameSpaceSockets = Object.keys(room.connected); // Get Object with Connected SocketIds as properties
+                    connectedNameSpaceSockets.forEach(socketId => {
+                        room.connected[socketId].disconnect(); // Disconnect Each socket
+                    });
+                    room.removeAllListeners(); // Remove all Listeners for the event emitter
+                    delete io.nsps[nsp];
+                    console.log(rooms)
+                    rooms[nsp.slice(1,nsp.length)] = 0
+                    console.log(rooms)
+                } else {
+                    var msg = 'leave...'
+                    room.emit('chat message', `${socket.nickName} ${msg}`)
+                }
+            })
+
+        })
+
+
+    })(io, nsp)
+
+
+}
+
+module.exports = {
     getTime_Str,
+    newchatroom,
 }
--- server.js
+++ server.js
@@ -7,10 +7,12 @@ var bodyParser = require('body-parser')
 var session = require('express-session')
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+
+
+var pool_connection = mysql.createPool({
     host: 'url',
-    user: 'user',
-    password: 'psw',
+    user: 'dbuser',
+    password: 'password',
     database: 'db',
 })
 
@@ -30,10 +32,14 @@ app.set('views', __dirname + '/public/views/')
 
 function dbopen(sSQL) {
     return new Promise(function (resolve, reject) {
-        connection.query(sSQL, function (err, rows, fields) {
-            if (err) throw err
-            resolve(rows)
+        pool_connection.getConnection(function (err, connection) {
+            connection.query(sSQL, function (err, rows, fields) {
+                connection.release();
+                if (err) throw err
+                resolve(rows)
+            })
         })
+
     })
 }
 
@@ -50,6 +56,7 @@ app.get('/signIn', function (req, res) {
     } else {
         res.render('signIn.pug')
     }
+
 })
 
 app.post('/signUp', async function (req, res) {
@@ -62,10 +69,11 @@ app.post('/signUp', async function (req, res) {
         await dbopen(`INSERT INTO user (name) VALUES ('${req.body.nickName}')`)
 
         res.render('signIn.pug', {
-            upinfo: '創見成功'
+            upinfo: '創建成功'
         })
     }
 })
+
 
 app.post('/signIn', async function (req, res) {
     var dbresult = await dbopen(`SELECT name FROM user WHERE name = '${req.body.nickName}'`)
@@ -73,6 +81,8 @@ app.post('/signIn', async function (req, res) {
         req.session.nickName = req.body.nickName
         res.render("chat.pug", {
             nickName: req.session.nickName,
+            random: false,
+            nsp: ""
         })
     } else {
         res.render('signIn.pug', {
@@ -80,49 +90,101 @@ app.post('/signIn', async function (req, res) {
         })
     }
 
+
 })
 app.post('/loadmsg', async function (req, res) {
     var time = apis.getTime_Str()
-    
+
     if (req.body.inputtime) {
-        time =req.body.inputtime
+        time = req.body.inputtime
         console.log(`time = ${time}`)
     }
-        var dbresult = await dbopen(`select c.message ,c.inputtime ,u.name from chat_message c ,user u where c.id = u.id and inputtime < '${time}' order by inputtime DESC Limit 30`)
-        console.log(dbresult.length)
+    var dbresult = await dbopen(`select c.message ,c.inputtime ,u.name from chat_message c ,user u where c.id = u.id and inputtime < '${time}' order by inputtime DESC Limit 30`)
+    console.log(dbresult.length)
     res.send(dbresult)
 })
 
+app.post('/randomChat', function (req, res) {
+    for (var key in rooms) {
+        if (rooms[key] < 2) {
+            if (rooms[key] == 0) {
+                apis.newchatroom(io, `/${key}`, rooms)
+            }
+            rooms[key]++
+            res.render('randomchat.pug', {
+                nickName: `user${rooms[key]}`,
+                random: true,
+                nsp: `/${key}`,
 
-io.on('connection', function (socket) {
+            })
+            break
+        }
+    }
+})
+
+var rooms = {
+    roomA: 0,
+    roomB: 0,
+    roomC: 0,
+    roomD: 0,
+    roomE: 0,
+    roomF: 0,
+    roomG: 0,
+    roomH: 0,
+    roomI: 0,
+    roomJ: 0,
+}
+
+function doubleSignin(generalroom, nickName) {
+    var socketids = Object.keys(generalroom.connected)
+    for (var i = 0; i < socketids.length; i++) {
+        if (generalroom.connected[socketids[i]].nickName == nickName) {
+            return true
+        }
+    }
+    return false
+}
+
+var generalroom = io.of('/general')
+generalroom.on('connection', function (socket) {
     var date = apis.getTime_Str()
     console.log(`[${new Date(date)}]a user come in!`)
 
     socket.on('welcome chat', async function (nickName) {
+        var flag = doubleSignin(generalroom, nickName)
         var dbresult = await dbopen(`SELECT * FROM user Where name ='${nickName}'`)
         socket['nickName'] = dbresult[0].name
         socket['db_id'] = dbresult[0].id
         var msg = `comming...`
         var inputtime = apis.getTime_Str()
-        await dbopen(`INSERT INTO chat_message (message,inputtime,id) VALUES ('${msg}','${inputtime}','${socket.db_id}')`)
-        io.sockets.emit('chat message', `${socket.nickName} ${msg}`)
+        
+        if (!flag) {
+            await dbopen(`INSERT INTO chat_message (message,inputtime,id) VALUES ('${msg}','${inputtime}','${socket.db_id}')`)
+            generalroom.emit('chat message', `${socket.nickName} : ${msg}`)
+        }
     })
 
     socket.on('chat message', async function (msg) {
         var inputtime = apis.getTime_Str()
         await dbopen(`INSERT INTO chat_message (message,inputtime,id) VALUES ('${msg}','${inputtime}','${socket.db_id}')`)
         console.log(`${socket.nickName} : ${msg}`)
-        io.sockets.emit('chat message', `${socket.nickName} : ${msg}`)
+        generalroom.emit('chat message', `${socket.nickName} : ${msg}`)
     })
 
     socket.on('disconnect', async function () {
         var inputtime = apis.getTime_Str()
         console.log(`[${new Date(inputtime)}]${socket.nickName} user offline!`)
         var msg = `leave....`
-        await dbopen(`INSERT INTO chat_message (message,inputtime,id) VALUES ('${msg}','${inputtime}','${socket.db_id}')`)
-        io.sockets.emit('chat message', `${socket.nickName} ${msg}`)
+        
+        if (!doubleSignin(generalroom, socket.nickName)) {
+            await dbopen(`INSERT INTO chat_message (message,inputtime,id) VALUES ('${msg}','${inputtime}','${socket.db_id}')`)
+            generalroom.emit('chat message', `${socket.nickName} : ${msg}`)
+        }
+
+
     })
 })
+
 
 http.listen(3000, function () {
     console.log("port 3000 listend.....")
